b051f7b12ecebe45d01b227194ef03cb97f416d3
atushi0624 <35013530+atushi0624@users.noreply.github.com>
2019-03-03 17:35:09 +0900

    Delete company_filters.py

--- company_filters.py
+++
@@ -1,9 +0,0 @@
-from django import template
- 
-register = template.Library()
- 
-@register.filter
-def company_ID(querydict):
-    company_ID = querydict.get('name')
- 
-    return "" if company_ID is None else company_ID
